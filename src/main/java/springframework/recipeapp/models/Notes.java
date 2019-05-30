e200f85c96fd1528fcc77e602e5ec0fae2622873
y1zhou <y1zhou@ucsd.edu>
2019-05-29 17:18:54 -0700

    "one to one JPA 139"

---
+++ src/main/java/springframework/recipeapp/models/Notes.java
@@ -0,0 +1,19 @@
+package springframework.recipeapp.models;
+
+
+import javax.persistence.*;
+
+public class Notes {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+}
