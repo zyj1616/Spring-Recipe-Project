2ee46dd875f51500601ffe971a416fbb691ea040
y1zhou <y1zhou@ucsd.edu>
2019-05-29 17:41:34 -0700

    "many to one JPA 140"

--- src/main/java/springframework/recipeapp/models/Recipe.java
+++ src/main/java/springframework/recipeapp/models/Recipe.java
@@ -1,6 +1,7 @@
 package springframework.recipeapp.models;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -23,6 +24,18 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
 
 
     public Long getId() {
